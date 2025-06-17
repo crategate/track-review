17a39e6db2ffa3047cf8afb61a5c8c20f7b788e1
michael pratt <michael@deb.lan>
2025-06-17 12:16:00 -0400

    init

---
+++ src/fetch-tracks.rs
@@ -0,0 +1,24 @@
+
+
+use std::fs;
+use std::io;
+use sted::path::{Path, PathBuf};
+
+pub struct Fetcher {
+    folder: PathBuf,
+    files: Vec<PathBuf>,
+    track_now: usize,
+}
+
+impl Fetcher {
+    pub fn new(folder: PathBuf) -> Self {
+        let mut fetching = Fetcher {
+            folder: folder.clone(),
+            files: Vec::new(),
+            track_now: usize,
+        };
+        fetching.load_tracks(folder);
+        fetching
+    }
+}
+
