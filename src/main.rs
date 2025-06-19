a24dc780cffbd50b7e7baeedc083740c10cfe0d6
michael pratt <michael@deb.lan>
2025-06-19 16:26:02 -0400

    derive

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+struct Cli {
+    path: Option<PathBuf>,
+}
+
 fn main() {
+    let cli = Cli::parse();
     println!("Hello, world!");
 }
