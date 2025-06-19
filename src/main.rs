722afe24b59a7c341322a65b87a849721b455fda
michael pratt <michael@deb.lan>
2025-06-19 16:36:46 -0400

    default path

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 use std::path::PathBuf;
 
 #[derive(Debug, Parser)]
+#[command(version, about, long_about = "Quickest Crates In the West")]
 struct Cli {
     path: Option<PathBuf>,
 }
@@ -9,4 +10,6 @@ struct Cli {
 fn main() {
     let cli = Cli::parse();
     println!("Hello, world!");
+
+    let path = cli.path.unwrap_or(PathBuf::from("~/Music/INCOMING"));
 }
